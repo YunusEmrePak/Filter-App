45874dcfbe55016946595d3b53018cf05437170c
Yunus Emre Pak <yunusemrepak@windowslive.com>
2023-01-17 14:46:59 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-function App() {
-  return (
+import Card from './components/Card';
 
-    <div>Hello World</div>
+
+const App = () => {
+
+  const objects = [
+    {
+        id: 1,
+        img: '1.png',
+        title: 'First',
+        explanation: 'Hello World 1'
+    },
+    {
+        id: 2,
+        img: '2.png',
+        title: 'Second',
+        explanation: 'Hello World 2'
+    },
+    {
+        id: 3,
+        img: '3.png',
+        title: 'Third',
+        explanation: 'Hello World 3'
+    },
+    {
+      id: 4,
+      img: '4.png',
+      title: 'Fourth',
+      explanation: 'Hello World 4'
+  }
+  ];
+
+  return (
+    <div className='container'>
+      <Card object={objects} i = {0} />
+      <Card object={objects} i = {1} />
+      <Card object={objects} i = {2} />
+      <Card object={objects} i = {3} />
+    </div>
     
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import './Card.css';
+
+const Card = props => {
+
+    const index = props.i;
+    const prps = props.object[index];
+
+    return (
+        <div className='card'>
+            <img src={require(`../img/${prps.img}`)} alt={index}/>
+            <div className='card-information'>
+                <h3>{prps.title}</h3>
+                <p className='card-explanation'>{prps.explanation}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
